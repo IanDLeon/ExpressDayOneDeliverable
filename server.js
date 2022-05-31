5c890c4360bbc991581520a47dbbcbf85241fb46
IanDLeon <ian.reggie.deleon@gmail.com>
2022-05-31 18:52:18 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ app.listen(port, () => {
 app.get('/greeting/:name', (req,res) => {
   res.send('Wow! Hello, ' + req.params.name)
 })
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  let percentage = parseInt(req.params.total) * parseInt(req.params.tipPercentage) /100
+
+  res.send("the percentage is " + percentage)
+})
